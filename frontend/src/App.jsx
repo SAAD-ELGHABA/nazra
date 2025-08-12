a71b5850ea66900ceaf47c0a4ce61b48451e62ee
SAAD-ELGHABA <saadlraba@gmail.com>
2025-08-12 15:38:56 +0100

    initializing narza

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="font-black text-xl">NAZRA</h1>
+    </div>
+  );
+}
+
+export default App;
