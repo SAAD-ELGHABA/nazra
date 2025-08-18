eaa2575a441b820a24100a0832aa7136c7a214e4
SAAD-ELGHABA <saadlraba@gmail.com>
2025-08-18 16:57:37 +0100

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="font-black text-xl">NAZRA</h1>
+      <h1 className="font-black text-5xl " style={{fontFamily:'monospace'}}>NAZRA.ma</h1>
     </div>
   );
 }
