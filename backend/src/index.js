47f8111cca9285fd37d09e7ca2e02581310eb5bc
SAAD-ELGHABA <saadlraba@gmail.com>
2025-09-13 13:02:51 +0100

    some changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 
 const app = express();
 
+
+
 app.use(cors({
-  origin: process.env.NODE_ENV === "production"
-    ? process.env.VITE_API_URL
-    : "http://localhost:5173",
+  origin: [process.env.FRONTEND_URL,process.env.VITE_API_URL,"http://localhost:3000","http://localhost:5173"],
   credentials: true,
 }));
 
