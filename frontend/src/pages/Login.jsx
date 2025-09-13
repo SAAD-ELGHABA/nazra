ef0993e421ae8679604613e2614b65db64a56ced
SAAD-ELGHABA <saadlraba@gmail.com>
2025-09-13 13:46:55 +0100

    some changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
-    const [dataUser,setDataUser] = useState({
-        // name:'',
-        email:'adil.nmili19@gmail.com',
-        password:'password123456'
-    })
-    const [error, setError] = useState('')
+  const [dataUser, setDataUser] = useState({
+    // name:'',
+    email: "adil.nmili19@gmail.com",
+    password: "password123456",
+  });
+  const [error, setError] = useState("");
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-       const response = await axios.post("http://localhost:5000/api/auth/login",dataUser)
-       if (response.status === 200){
-        localStorage.setItem('User_Data',JSON.stringify(response.data))
-        // localStorage.setItem('User_Data_token',JSON.stringify(response.data.token))
-        localStorage.setItem('User_Data_token', response.data.token);
-        navigate('/dashboard')
-       }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/auth/login`,
+      dataUser
+    );
+    if (response.status === 200) {
+      localStorage.setItem("User_Data", JSON.stringify(response.data));
+      // localStorage.setItem('User_Data_token',JSON.stringify(response.data.token))
+      localStorage.setItem("User_Data_token", response.data.token);
+      navigate("/dashboard");
     }
-
+  };
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-white">
-        {
-            error && (
-                <p className='text-red-600 py-2 px-6  border border-red-600 bg-red-600/50 text-center capitalize text-sm'>{error}</p>
-            )
-        }
+      {error && (
+        <p className="text-red-600 py-2 px-6  border border-red-600 bg-red-600/50 text-center capitalize text-sm">
+          {error}
+        </p>
+      )}
       <div className="w-full max-w-md p-8 rounded-2xl shadow-lg border border-black/10">
         {/* Title */}
         <h2 className="text-3xl font-bold text-black text-center mb-6">
@@ -60,7 +61,9 @@ export default function LoginPage() {
             <input
               type="email"
               value={dataUser.email}
-              onChange={e => setDataUser({...dataUser,email:e.target.value})}
+              onChange={(e) =>
+                setDataUser({ ...dataUser, email: e.target.value })
+              }
               placeholder="Enter your email"
               className="w-full px-4 py-2 border border-black/20 rounded-lg focus:outline-none focus:ring-2 focus:ring-black bg-white text-black placeholder-gray-400"
             />
@@ -74,7 +77,9 @@ export default function LoginPage() {
             <input
               type="password"
               value={dataUser.password}
-              onChange={e => setDataUser({...dataUser,password:e.target.value})}
+              onChange={(e) =>
+                setDataUser({ ...dataUser, password: e.target.value })
+              }
               placeholder="Enter your password"
               className="w-full px-4 py-2 border border-black/20 rounded-lg focus:outline-none focus:ring-2 focus:ring-black bg-white text-black placeholder-gray-400"
             />
