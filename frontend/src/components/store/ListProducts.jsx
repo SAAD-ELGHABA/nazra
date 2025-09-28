411c528e52100982a2157f3f82102c2c63d64505
Adil Nmili <adile.namili98@gmail.com>
2025-09-28 10:10:48 +0100

    update the chakeout and card components

--- frontend/src/components/store/ListProducts.jsx
+++ frontend/src/components/store/ListProducts.jsx
@@ -9,7 +9,7 @@ import ProductComponent from "./ProductComponent";
 
 function ListProducts({ products }) {
   const [currentPage, setCurrentPage] = useState(1);
-  const productsPerPage = 8;
+  const productsPerPage = 12;
 
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
